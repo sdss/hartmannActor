32722c152253259c089bdd8a1d408da673ea115d
parejkoj <parejkoj@AlbireoPro.local>
2014-09-18 22:24:40 +0000

    Made profile_hartmann work with new code.

--- test_hartmannActor/profile_hartmann.py
+++ test_hartmannActor/profile_hartmann.py
@@ -4,41 +4,33 @@ profile boss_collimate to find bottlenecks.
 import cProfile
 import pstats
 import time
+import ConfigParser
 
-from sopActor.utils import boss_collimate
+from actorcore import TestHelper
 
-class Cmd(object):
-    def __init__(self):
-        """Save the level of any messages that pass through."""
-        self.messages = ''
-    def _msg(self,txt,level):
-        print level,txt
-        self.messages += level
-    def inform(self,txt):
-        self._msg(txt,'i')
-    def diag(self,txt):
-        self._msg(txt,'d')
-    def warn(self,txt):
-        self._msg(txt,'w')
-    def fail(self,txt):
-        self._msg(txt,'f')
-    def error(self,txt):
-        self._msg(txt,'e')
+from hartmannActor import boss_collimate, hartmannActor_main
 
-cmd = Cmd()
-hart = boss_collimate.Hartmann()
+config = ConfigParser.ConfigParser()
+config.read('../etc/hartmann.cfg')
+m,b = hartmannActor_main.get_collimation_constants(config)
+
+cmd = TestHelper.Cmd(verbose=True)
+hart = boss_collimate.Hartmann(None, m, b)
 hart.spec = 'sp1'
 hart.cmd = cmd
 
+
+# NOTE: this isn't going to tell us anything, because all the work happens
+# inside the 4 processes, and Profile doesn't tell us anything about that.
+# Need to profile a single OneCam call if we really want to learn what's up.
 prof = cProfile.Profile()
-#prof.runcall(hart.collimate,cmd,165006)#,plot=True)
-prof.runcall(hart.do_one_cam,'b1','/data/spectro/*/','sdR-%s-%08d.fit*',165006,165007,False)#,plot=True)
+prof.runcall(hart.collimate,165006,indir='data/',cmd=cmd,moveMotors=False)
 prof.dump_stats('hartmann.prof')
 p = pstats.Stats('hartmann.prof')
 p.strip_dirs()
 p.sort_stats('time').print_stats(10)
 
 t1 = time.time()
-hart.collimate(cmd,165190)
+hart.collimate(165006, indir='data/', cmd=cmd, moveMotors=False)
 t2 = time.time()
 print 'time:',t2-t1
