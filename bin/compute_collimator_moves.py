87940807d057f5362ce292a1b58899e0399388e7
parejkoj <parejkoj@AlbireoPro.local>
2014-09-19 00:23:18 +0000

    Added m and b cmdline options for playing with boss_collimate

--- bin/compute_collimator_moves.py
+++ bin/compute_collimator_moves.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-"""Compute the collimator moves for an already-taken Hartmann pair."""
+"""
+Compute the collimator moves for an already-taken Hartmann pair.
+
+Defaults to using etc/hartmann.cfg for the slope and intercept, unless -m or -b are specified.
+"""
 
 import sys, os
 import argparse
@@ -18,7 +22,12 @@ def main(argv=None):
     parser = argparse.ArgumentParser(description=__doc__, prog=os.path.basename(sys.argv[0]))
     parser.add_argument('FILE1', metavar='FILE1', type=str,
                         help='The first hartmann file (generally left).')
-
+    parser.add_argument('FILE2', metavar='FILE2', default=None, nargs='?',
+                        help='Optional second hartmann file (generally right).')
+    parser.add_argument('-m',default=None, dest='m',
+                        help='Slope of the offset->motor function: b1,r1,b2,r2.')
+    parser.add_argument('-b',default=None, dest='b',
+                        help='Intercept of the offset->motor function: b1,r1,b2,r2.')
     args = parser.parse_args()
 
     exp1 = args.FILE1
@@ -29,6 +38,12 @@ def main(argv=None):
     config = ConfigParser.ConfigParser()
     config.read(os.environ['HARTMANNACTOR_DIR']+'/etc/hartmann.cfg')
     m,b = hartmannActor_main.get_collimation_constants(config)
+
+    cams = ['b1','r1','b2','r2']
+    if args.m is not None:
+        m = dict(zip(cams, [float(x) for x in args.m.split(',')]))
+    if args.b is not None:
+        b = dict(zip(cams, [float(x) for x in args.b.split(',')]))
 
     hart = boss_collimate.Hartmann(None, m, b)
 
