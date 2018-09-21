c57c3b02271e609d235765cd98cc18cd87a51d83
José Sánchez-Gallego <gallegoj@uw.edu>
2018-09-21 10:59:54 -0600

    Clean up hartmannCmd

--- python/hartmannActor/Commands/hartmannCmd.py
+++ python/hartmannActor/Commands/hartmannCmd.py
@@ -13,40 +13,33 @@ from hartmannActor import boss_collimate
 
 
 class hartmannCmd(object):
-    '''Wrap commands to the hartmann actor'''
+    """Wrap commands to the hartmann actor"""
 
     def __init__(self, actor):
+
         self.actor = actor
-        #
+
         # Declare commands
-        #
         self.keys = keys.KeysDictionary(
-            'hartmann_hartmann',
-            (1, 1),
-            keys.Key('id', types.Int(), help='first exposure number of Hartmann pair to process.'),
-            keys.Key(
-                'id2',
-                types.Int(),
-                help='second exposure number of Hartmann to process (default: id+1).'),
-            keys.Key(
-                'mjd',
-                types.Int(),
-                help='MJD of the Hartmann pair to process (default: current MJD).'),
-            keys.Key('noCorrect', help='if set, do not apply any recommended corrections.'),
-            keys.Key('noSubframe', help='if set, take fullframe images.'),
-            keys.Key(
-                'ignoreResiduals',
-                help='if set, apply red moves regardless of resulting blue residuals.'),
-            keys.Key(
-                'noCheckImage',
-                help=
-                'if set, do not check the flux level in the image (useful for sparse plugged plates).'
-            ),
-            keys.Key(
-                'minBlueCorrection',
-                help='if set, the calculated correction for the '
-                'blue ring will be the minimum to get in the '
-                'tolerance range.'),
+            'hartmann_hartmann', (1, 1),
+            keys.Key('id', types.Int(),
+                     help='first exposure number of Hartmann pair to process.'),
+            keys.Key('id2', types.Int(),
+                     help='second exposure number of Hartmann to process (default: id+1).'),
+            keys.Key('mjd', types.Int(),
+                     help='MJD of the Hartmann pair to process (default: current MJD).'),
+            keys.Key('noCorrect',
+                     help='if set, do not apply any recommended corrections.'),
+            keys.Key('noSubframe',
+                     help='if set, take fullframe images.'),
+            keys.Key('ignoreResiduals',
+                     help='if set, apply red moves regardless of resulting blue residuals.'),
+            keys.Key('noCheckImage',
+                     help='if set, do not check the flux level in the image '
+                          '(useful for sparse plugged plates).'),
+            keys.Key('minBlueCorrection',
+                     help='if set, the calculated correction for the blue ring will be '
+                          'the minimum to get in the tolerance range.'),
         )
 
         self.vocab = [
@@ -59,12 +52,12 @@ class hartmannCmd(object):
         ]
 
     def ping(self, cmd):
-        '''Query the actor for liveness/happiness.'''
+        """Query the actor for liveness/happiness."""
         cmd.inform('status=%s' % myGlobals.hartmannStatus)
         cmd.finish("text='Pong'")
 
     def status(self, cmd, finish=True):
-        '''Report status and version; obtain and send current data'''
+        """Report status and version; obtain and send current data"""
 
         self.actor.sendVersionKey(cmd)
         cmd.inform('status=%s' % myGlobals.hartmannStatus)
@@ -72,15 +65,18 @@ class hartmannCmd(object):
             cmd.finish()
 
     def recompute(self, cmd):
-        """ Reduce a given pair of already taken exposures. """
+        """Reduce a given pair of already taken exposures."""
+
         hartmann = myGlobals.hartmann
         keywords = cmd.cmd.keywords
+
         expnum1 = int(keywords['id'].values[0])
         expnum2 = int(keywords['id2'].values[0]) if 'id2' in keywords else None
+
         if 'mjd' in keywords:
             mjd = int(keywords['mjd'].values[0])
         else:
-            # SDSS MJD is truncaded (MJD_TAI + 0.3)
+            # SDSS MJD is truncated (MJD_TAI + 0.3)
             mjd = int(astroMJD.mjdFromPyTuple(time.gmtime()) + 0.3)
 
         moveMotors = 'noCorrect' not in keywords
@@ -98,10 +94,13 @@ class hartmannCmd(object):
             cmd.fail('text="Collimation process failed"')
 
     def collimate(self, cmd):
+        """Take and reduce a pair of hartmann exposures.
+
+        Assumes the arc lamps are already on. Apply the recommended collimator
+        moves unless noCorrect is specified.
+
         """
-        Take and reduce a pair of hartmann exposures, assuming the arc lamps are already on.
-        Apply the recommended collimator moves unless noCorrect is specified.
-        """
+
         hartmann = myGlobals.hartmann
         moveMotors = 'noCorrect' not in cmd.cmd.keywords
         subFrame = 'noSubframe' not in cmd.cmd.keywords
@@ -114,6 +113,7 @@ class hartmannCmd(object):
             return
 
         hartmann.reinit()
+
         hartmann(
             cmd,
             moveMotors=moveMotors,
@@ -121,6 +121,7 @@ class hartmannCmd(object):
             ignoreResiduals=ignoreResiduals,
             noCheckImage=noCheckImage,
             minBlueCorrection=minBlueCorrection)
+
         if hartmann.success:
             cmd.finish()
         else:
