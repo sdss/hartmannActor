96d31082f38ee0e1bdcef222d8aa12cc1a249c1e
José Sánchez-Gallego <gallegoj@uw.edu>
2019-10-18 14:51:34 -0700

    Specify what spec is skipping blue ring correction

--- python/hartmannActor/boss_collimate.py
+++ python/hartmannActor/boss_collimate.py
@@ -789,7 +789,7 @@ class Hartmann(object):
         rres = self.result[spec]['r'] - avg
 
         if np.any(np.isnan(self.result[spec].values())):
-            self.cmd.warn('text="bres is nan, skipping blue ring correction,"')
+            self.cmd.warn('text="{}: bres is nan, skipping blue ring correction,"'.format(spec))
             bres = 0.
 
         # Calculates the minimum blue ring correction needed to get in the
